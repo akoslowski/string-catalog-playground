b50f7dc83388c9de8a3cd528fa3eddc19afd4b31
akoslowski <2005824+akoslowski@users.noreply.github.com>
2024-02-20 11:06:31 +0100

    Initial Commit

---
+++ LocalizedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizedApp
+//
+//  Created by Andreas Koslowski on 20.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalizedApp/LocalizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedAppApp.swift
+//  LocalizedApp
+//
+//  Created by Andreas Koslowski on 20.02.24.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
